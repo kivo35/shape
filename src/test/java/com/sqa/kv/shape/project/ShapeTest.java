a2118fe7c0a058e877f6bc98f4c8f7f663079364
Kirill <kivo35@hotmail.com>
2016-02-02 16:11:40 -0800

    add test methods to test Shape class

---
+++ src/test/java/com/sqa/kv/shape/project/ShapeTest.java
@@ -0,0 +1,15 @@
+package com.sqa.kv.shape.project;
+
+import org.junit.Test;
+
+import com.sqa.kv.shape.project.Shape;
+
+public class ShapeTest
+{
+	@Test
+	public void test()
+	{
+		Shape myShape = new Shape();
+		System.out.println(myShape);
+	}
+}
